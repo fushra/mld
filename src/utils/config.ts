25c96d89f840e2737b9d698f7852d2c6ae8b974f
trickypr <trickypr@icloud.com>
2021-07-01 17:15:18 +1000

    ✨ Use codesplitting by default

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,11 +1,15 @@
 import { readFileSync } from 'fs'
+import { MemlCore } from 'meml'
 import { join } from 'path'
+import { getLastWorkingDir } from './fs'
 
 export const defaultConfig: Config = {
   pages: [],
   srcDir: 'src',
   out: 'build',
   publicDir: 'public',
+  codeSplitting: true,
+  root: '/',
   devServer: {
     port: 8080,
   },
@@ -13,13 +17,29 @@ export const defaultConfig: Config = {
 
 export interface Config {
   pages: string[]
-  srcDir?: string
-  out?: string
-  publicDir?: string
-  devServer?: {
+  srcDir: string
+  out: string
+  publicDir: string
+  /**
+   * If files should be split or embedded inside files.
+   *
+   * @default true
+   */
+  codeSplitting: boolean
+  /**
+   * Where the root of the webpage is. This is most important to be set with
+   * `codeSplitting` enabled
+   *
+   * @default '/'
+   */
+  root: string
+  devServer: {
     port: number
   }
 }
+
+// If the config has been applied to the system
+let _appliedConfig = false
 
 export const getConfig = (path: string): Config => {
   const fileString = readFileSync(join(path, 'app.json')).toString()
@@ -31,5 +51,23 @@ export const getConfig = (path: string): Config => {
     )
   }
 
-  return { ...defaultConfig, ...parsed } as Config
+  const config = { ...defaultConfig, ...parsed } as Config
+
+  if (!_appliedConfig) {
+    applyConfig(config)
+    _appliedConfig = true
+  }
+
+  return config
+}
+
+/**
+ * Applies config changes to the meml transpiler
+ *
+ * @param config The project config
+ */
+export const applyConfig = (config: Config) => {
+  MemlCore.rootPath = config.root
+  MemlCore.shouldLink = config.codeSplitting
+  MemlCore.distPath = join(getLastWorkingDir(), config.out)
 }
--- src/utils/fs.ts
+++ src/utils/fs.ts
@@ -1,11 +1,21 @@
 import { join } from 'path'
 
+// Store the last working directory to be accessed latter
+let _lastWorkingDir: string
+
 export const workingDir = process.cwd()
 
-export const currentOrPath = (path?: string) => {
+const _currentOrPath = (path?: string) => {
   if (typeof path != 'undefined') {
     return join(workingDir, path)
   }
 
   return workingDir
 }
+
+export const currentOrPath = (path?: string) => {
+  _lastWorkingDir = _currentOrPath(path)
+  return _lastWorkingDir
+}
+
+export const getLastWorkingDir = () => _lastWorkingDir
